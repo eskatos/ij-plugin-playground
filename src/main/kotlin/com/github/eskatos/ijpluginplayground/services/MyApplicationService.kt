6d53d42f6a95ef9b721fbb46c5037bc43fc954e8
GitHub Action <action@github.com>
2021-07-09 12:47:29 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/eskatos/ijpluginplayground/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.eskatos.ijpluginplayground.services
+
+import com.github.eskatos.ijpluginplayground.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/eskatos/ijpluginplayground/services/MyProjectService.kt
+++ src/main/kotlin/com/github/eskatos/ijpluginplayground/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.eskatos.ijpluginplayground.services
 
+import com.github.eskatos.ijpluginplayground.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
